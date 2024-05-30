0a98c4193f460d4e5662370da671fa2c9a22ed3b
Ashutosh Singh <ashutoshsingh.45987@gmail.com>
2024-05-30 16:59:53 +0530

    Navbar functionality and shadcn sheet usage

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -5,17 +5,15 @@ import React, { Children, ReactNode } from "react";
 const HomeLayout = ({children}:{children:ReactNode}) => {
   return(
     <main className="relative">
-
-      <Navbar/>
+      <Navbar />
       <div className="flex">
         <Sidebar/>
-        <section className=" flex min-h-screen flex-1 flex-col px-6 pb-6 pt-26 max-md:pb-14 sm:px-14">
+        <section className=" flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
             <div className=" w-full">
                   {children}
             </div>
         </section>
       </div>
-      
     </main>
   );
 };
